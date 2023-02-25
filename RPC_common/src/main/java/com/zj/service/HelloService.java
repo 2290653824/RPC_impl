d21b38384b00716d00751be4f8417ddea58a0d49
zhengjian <zhengjian@xiaohongshu.com>
2023-02-25 20:57:12 +0800

    test

---
+++ RPC_common/src/main/java/com/zj/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.zj.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+
+public class HelloService {
+
+
+
+    private HelloService helloService;
+
+    public String startHello(){
+        return helloService.startHello();
+    }
+}
---
+++ RPC_service/src/com/zj/service/service/HelloService.java
@@ -0,0 +1,7 @@
+package com.zj.service.service;
+
+
+public interface HelloService {
+
+    String hello();
+}
